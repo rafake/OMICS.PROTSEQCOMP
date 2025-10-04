fae305f299b7c1d0a53dbbc95fc4f1b804a5dba0
Rafał Kelm <rkelm@keraq.com>
2025-10-04 17:38:12 +0200

    add debugging

--- test_script.py
+++ test_script.py
@@ -2,3 +2,13 @@
 # Simple test to verify Python execution
 
 print("Hello World!")
+
+# Print current working directory
+cwd = os.getcwd()
+print("Current working directory:", cwd)
+
+# List files in the current directory
+files = os.listdir(cwd)
+print("Files in directory:")
+for f in files:
+    print("  -", f)
