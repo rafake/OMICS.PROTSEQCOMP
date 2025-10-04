1deb4c37b3359a35d8c2753c3fc79d83fd16a8e0
Rafał Kelm <rkelm@keraq.com>
2025-10-04 18:43:58 +0200

    add spark container

---
+++ mus_sampling_batch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -J sampling-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=10
+#SBATCH --partition topola
+#SBATCH --time=00:05:00
+#SBATCH -A g100-2238
+
+APPTAINER=$HOME/zadanie/1_environment/apptainer_local/bin/apptainer
+
+# Run Spark container with sampling commands
+$APPTAINER exec docker://apache/spark spark-submit mus_musculus_sampling.py
+
+echo "Mus musculus sampling job completed!"
