0f9171017bb9f952428ed1c371cfe8b9d34ba979
Rafał Kelm <rkelm@keraq.com>
2025-10-04 18:04:53 +0200

    unbuffered

--- test_script.py
+++ test_script.py
@@ -1,8 +1,1 @@
-import os
-
-# Prepare an output file in the current directory
-out_file = os.path.join(os.getcwd(), "test_output.txt")
-with open(out_file, "w") as f:
-    # Print and save at the same time, flushing stdout immediately
-    print("Hello World!", flush=True)
-    print("Hello World!", file=f, flush=True)
+print("Hello World!", flush=True)
