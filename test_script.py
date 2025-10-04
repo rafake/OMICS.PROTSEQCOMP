4a4d410139ca4b0a18e39a6062c9336e5e52ab49
Rafał Kelm <rkelm@keraq.com>
2025-10-04 18:21:05 +0200

    save to file

--- test_script.py
+++ test_script.py
@@ -1,1 +1,7 @@
 print("Hello World!", flush=True)
+
+# Save hello world to file
+with open("hello_world_output.txt", "w") as f:
+    f.write("Hello World!")
+
+print("Message saved to hello_world_output.txt", flush=True)
